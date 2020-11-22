e97a9ffbc0200bd40feeffb684f6ff7240dff860
DevinAlexander <devinmalexander@gmail.com>
2020-11-22 13:20:58 -0600

    [CHORE] Get list of data on books page

--- app/routes/books.js
+++ app/routes/books.js
@@ -1,4 +1,10 @@
 import Route from '@ember/routing/route';
 
 export default class BooksRoute extends Route {
+    async model() {
+        const api_url = 'https://anapioficeandfire.com/api/books';
+        const response = await fetch(api_url);
+        const json = await response.json();
+        return json;
+    }
 }
