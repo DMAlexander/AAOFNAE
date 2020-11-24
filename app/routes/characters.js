17049f3aee79d8882c38bdc0fe9c69c5bc4c7236
devin.alexander <devin.alexander@coschedule.com>
2020-11-23 20:51:41 -0600

    [CHORE] Characters template

--- app/routes/characters.js
+++ app/routes/characters.js
@@ -2,7 +2,7 @@ import Route from '@ember/routing/route';
 
 export default class CharactersRoute extends Route {
     async model() {
-        const api_url = 'https://anapioficeandfire.com/api/characters?&pageSize=100';
+        const api_url = 'https://anapioficeandfire.com/api/characters?&pageSize=5';
         const response = await fetch(api_url);
         const json = await response.json();
         return json;
