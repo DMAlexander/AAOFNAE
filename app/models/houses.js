a96563d8e70f783129166a563cd9a5479bcd7926
devin.alexander <devin.alexander@coschedule.com>
2020-11-28 22:10:36 -0600

    [CHORE] Created models classes for books, characters, and houses

---
+++ app/models/houses.js
@@ -0,0 +1,20 @@
+import Model, { attr } from '@ember-data/model';
+
+export default class HousesModel extends Model {
+    @attr url;
+    @attr name;
+    @attr region;
+    @attr coatOfArms;
+    @attr words;
+    @attr titles;
+    @attr seats;
+    @attr currentLord;
+    @attr heir;
+    @attr overlord;
+    @attr founded;
+    @attr founder;
+    @attr diedOut;
+    @attr ancestralWeapons;
+    @attr cadetBranches;
+    @attr swornMembers;
+}
