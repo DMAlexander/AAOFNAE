abbb2df174dd3d424c47e889c847c79c211fd958
DevinAlexander <devinmalexander@gmail.com>
2020-11-30 06:52:58 -0600

    [CHORE] Code Refactoring

--- app/routes/house.js
+++ app/routes/house.js
@@ -1,6 +1,8 @@
 import Route from '@ember/routing/route';
+import { inject as service } from '@ember/service';
 
 export default class HouseRoute extends Route {
+    @service store;
     async model() {
         const data = this.store.peekAll('houses');
         return data;
