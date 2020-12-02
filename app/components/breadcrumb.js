67f87251bd09290ed0c531a8baef790f07e509ae
DevinAlexander <devinmalexander@gmail.com>
2020-12-02 17:15:47 -0600

    [CHORE] Show breadcrumb on all screens

--- app/components/breadcrumb.js
+++ app/components/breadcrumb.js
@@ -1,4 +1,20 @@
 import Component from '@glimmer/component';
+import { inject as service } from '@ember/service';
 
 export default class BreadcrumbComponent extends Component {
+    @service store;
+    get books() {
+        const books = this.store.peekAll('books');
+        return books;
+    }
+
+    get characters() {
+        const characters = this.store.peekAll('characters');
+        return characters;
+    }
+
+    get houses() {
+        const houses = this.store.peekAll('houses');
+        return houses;
+    }
 }
