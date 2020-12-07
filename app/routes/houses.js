02a9242ecba741743b7968e551a81f21df717918
DevinAlexander <devinmalexander@gmail.com>
2020-12-06 18:03:06 -0600

    [CHORE] Fixed up houses

--- app/routes/houses.js
+++ app/routes/houses.js
@@ -3,7 +3,7 @@ import { inject as service } from '@ember/service';
 
 export default class HousesRoute extends Route {
     @service store;
-    async model() {
+    async model(params) {
         const data = this.store.peekAll('houses');
         return data;
     }
