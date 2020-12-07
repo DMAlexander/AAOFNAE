b97704627b06248d5ced5bfac207822bc3c29175
DevinAlexander <devinmalexander@gmail.com>
2020-12-06 20:13:46 -0600

    [CHORE] Got a working breadcrumb

--- app/components/breadcrumb.js
+++ app/components/breadcrumb.js
@@ -14,4 +14,46 @@ export default class BreadcrumbComponent extends Component {
         const inHome = this.router.currentRouteName === 'home' ? true : false;
         return inHome;
     }
+
+    //Get all record data so we can iterate through it
+    get recordData() {
+        if(this.router.currentURL.includes('/books')) {
+            return this.store.peekAll('books');
+        }
+        if(this.router.currentURL.includes('/characters')) {
+           return this.store.peekAll('characters');
+        }
+        if(this.router.currentURL.includes('/houses')) {
+            return this.store.peekAll('houses');
+        }
+        return '';
+    }
+
+    get recordName() {
+        if(!this.args.baseRoute) {
+            return this.args.record?.name || this.args.record?.aliases;
+        }
+        return 'Record';
+    }
+
+    get showBooks() {
+        if(this.router.currentURL.includes('/books')) {
+            return true;
+        }
+        return null;
+    }
+
+    get showCharacters() {
+        if(this.router.currentURL.includes('/characters')) {
+            return true;
+        }
+        return null;
+    }
+
+    get showHouses() {
+        if(this.router.currentURL.includes('/houses')) {
+            return true;
+        }
+        return null;
+    }
 }
